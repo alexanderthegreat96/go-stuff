194d1a9cdfef19c6eab72462ba4ab4da5fd8e133
Lupaescu Alexandru <alexandru.lupaescu@scania.com>
2023-10-10 16:45:04 +0300

    adding test-module and cross compilation examples

---
+++ test-module/go.mod
@@ -0,0 +1,3 @@
+module github.com/alexanderthegreat96/go-stuff/test-module
+
+go 1.21.1
---
+++ test-module/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// to check the go env
+// run in terminal
+// go env GOARCH GOOS
+
+// build commands
+// go build . //builds for the current architecture
+// bash commands
+// GOOS=darwin go build // mac
+// GOOS=amd64 go build // windows
+// GOOS=linux GOARCH=amd64 go build
+
+// windows terminal / shell
+// go build -o DIR_PATH_HERE/filename.exe program
+// in the powershell / cmd promp we use $env: to declare GO ENV variables
+// $env:GOOS="linux"; $env:GOARCH="amd64"; go build -o binaries/filename_amd64_linux
+
+func main() {
+	fmt.Println("Hello world. This is the first test-module")
+}
