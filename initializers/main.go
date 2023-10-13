8aa4fce6ab8679ed90325c4baaf2dad1aeda885c
Lupaescu Alexandru <alexandru.lupaescu@scania.com>
2023-10-13 19:54:44 +0300

    adding intializers example. implementing and fixing donut.c in Go.

---
+++ initializers/main.go
@@ -0,0 +1,29 @@
+package main
+
+// this example is aimed at showcasing
+// the use of initializers
+
+// initializer or intializers are functions
+// that use the keyword init() -> func init() {}
+// they run before the main() -> func main() {}
+// meaning that normally, the program starts in
+// package main and runs in the func main() {}
+// func init() {} runs before func main() {}
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alexanderthegreat96/go-stuff/initializers/donut"
+)
+
+func init() {
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("Jan 02, 2006 15:03")
+	fmt.Println("Program started at: " + formattedTime)
+}
+
+func main() {
+	fmt.Println("Drawning a rotating sphere...")
+	donut.RenderDonut()
+}
